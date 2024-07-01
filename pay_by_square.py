05c62bc2f15dd500afc174356609f01e77a4a454
Ivan Labáth <Ivan.Labath@monoid.sk>
2024-07-01 12:34:53 +0200

    add command-line parsing

--- pay_by_square.py
+++ pay_by_square.py
@@ -1,7 +1,9 @@
+from typing import cast, Optional, Union
+
 import lzma
 import binascii
-from typing import Optional
 from datetime import datetime, date
+import argparse
 
 
 def generate(
@@ -18,6 +20,7 @@ def generate(
     note: str = '',
     beneficiary_address_1: str = '',
     beneficiary_address_2: str = '',
+    **kwds
 ) -> str:
     '''Generate pay-by-square code that can by used to create QR code for
     banking apps
@@ -153,30 +156,110 @@ def decode(pbs: str):
     res['recurring'] = split_data[14]
     assert split_data[15] == '0'  # not 'inkaso'
     res['inkaso'] = split_data[15]
-    res['beneficiary_name'] = split_data[16]
-    res['beneficiary_address_1'] = split_data[17]
-    res['beneficiary_address_2'] = split_data[18]
+    try:
+        res['beneficiary_name'] = split_data[16]
+        res['beneficiary_address_1'] = split_data[17]
+        res['beneficiary_address_2'] = split_data[18]
+    except IndexError:
+        pass  # Optional fields
     return res
 
 
-if __name__ == '__main__':
-    try:
-        import qrcode
-    except ImportError:
-        raise SystemExit('Install \'qrcode\' module to run demo')
+class ArgsEncode:
+    qrcode: bool
+    amount: float
+    iban: str
+    swift: str = ''
+    date: Optional[date] = None
+    beneficiary_name: str = ''
+    currency: str = 'EUR'
+    variable_symbol: str = ''
+    constant_symbol: str = ''
+    specific_symbol: str = ''
+    note: str = ''
+    beneficiary_address_1: str = ''
+    beneficiary_address_2: str = ''
 
-    code = generate(
-        amount=1,
-        iban='SK7700000000000000000000',
-        swift='FIOZSKBAXXX',
-        variable_symbol='11',
-        constant_symbol='22',
-        specific_symbol='33',
-        beneficiary_name='Foo',
-        beneficiary_address_1='address 1',
-        beneficiary_address_2='address 2',
-        note='bar',
-    )
+
+class ArgsDecode:
+    code: str
+
+
+# Note: also has func(Args..)
+ArgsType = Union[ArgsEncode, ArgsDecode]
+
+
+def generator(args: ArgsEncode):
+    gen_qrcode = args.qrcode
+    code = generate(**vars(args))
     print(code)
-    img = qrcode.make(code)
-    img.show()
+    if gen_qrcode:
+        try:
+            import qrcode
+        except ImportError:
+            raise SystemExit('Install python \'qrcode\' module show qr-code')
+        img = qrcode.make(code)
+        img.show()
+
+
+def decoder(args: ArgsDecode):
+    kv = decode(args.code)
+    print(kv)
+
+
+def get_args() -> ArgsEncode:
+    parser = argparse.ArgumentParser(
+            description=('Pay-by-square encoder and decoder.'
+                         'Implements non-recurring simple payment.'))
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    # encode
+    parser_encode = subparsers.add_parser('encode', help='Encode a pay-by-square payment')
+    parser_encode.add_argument('--amount', type=float)
+    parser_encode.add_argument('--iban', type=str)
+    parser_encode.add_argument('--swift', type=str, default='')
+    parser_encode.add_argument('--date', type=str) # TODO: Optional[date]
+    parser_encode.add_argument('--beneficiary_name', type=str, default='')
+    parser_encode.add_argument('--currency', type=str, default='EUR')
+    parser_encode.add_argument('--variable_symbol', type=str, default='')
+    parser_encode.add_argument('--constant_symbol', type=str, default='')
+    parser_encode.add_argument('--specific_symbol', type=str, default='')
+    parser_encode.add_argument('--note', type=str, default='')
+    parser_encode.add_argument('--beneficiary_address_1', type=str, default='')
+    parser_encode.add_argument('--beneficiary_address_2', type=str, default='')
+    parser_encode.add_argument('--qrcode', '-q', action='store_true', help='Generate and display qr-code')
+    parser_encode.set_defaults(func=generator)
+
+    # decode
+    parser_decode = subparsers.add_parser('decode', help='Decode a pay-by-square payment')
+    parser_decode.add_argument('code', type=str, help='Payment string, e.g. output from a qr-code-scanner')
+    parser_decode.set_defaults(func=decoder)
+
+    args1 = parser.parse_args()
+    args = cast(ArgsType, args1)
+    return args1
+
+
+def gen_sample(args: ArgsEncode) -> None:
+    args = ArgsType()
+    args.qrcode = True
+    args.amount=1
+    args.iban='SK7700000000000000000000'
+    args.swift='FIOZSKBAXXX'
+    args.variable_symbol='11'
+    args.constant_symbol='22'
+    args.specific_symbol='33'
+    args.beneficiary_name='Foo'
+    args.beneficiary_address_1='address 1'
+    args.beneficiary_address_2='address 2'
+    args.note='bar'
+    generate(args)
+
+
+def main() -> None:
+    args = get_args()
+    args.func(args)
+
+
+if __name__ == '__main__':
+    main()
